7370dfa11534b9a2114b9d7e354bc218171bebd7
jaretpwatkins <jaretpwatkins@users.noreply.github.com>
2017-04-26 09:16:16 -0500

    Test

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,20 +5,52 @@ def show
 
 id_number = params["the_id_number"]
 
-p = Photo.find(id_number)
+@photo = Photo.find(id_number)
 
-@the_caption = p.caption
+@the_caption = @photo.caption
 
-@the_image_url = p.source
-@created_at = p.created_at
+@the_image_url = @photo.source
+@created_at = @photo.created_at
 
 render("photos/show.html.erb")
 end
 
 def index
-  @all_photos = Photo.all
-  
+  @all_photos = Photo.order({:created_at => :desc })
+
   render("photos/index.html.erb")
+end
+
+def new_form
+
+  render("photos/new.html.erb")
+end
+
+def create_row
+
+  #render("photos/create.html.erb")
+
+  p = Photo.new
+  p.source = params["the_image_url"]
+  p.caption = params["the_caption"]
+  p.save
+  redirect_to("http://localhost:3000/photos")
+end
+
+def edit_form
+@photo = Photo.find(params[:the_id])
+  render("/photos/edit_form.html.erb")
+end
+
+def update_row
+
+  p = Photo.find(params[:the_id])
+
+  p.source = params["the_image_url"]
+  p.caption = params["the_caption"]
+  p.save
+
+  redirect_to("http://localhost:3000/photos")
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-get("/photos/:the_id_number", { :controller => "photos", :action => "show"})
+#create
+get("/photos/new", { :controller => "photos", :action => "new_form"})
+get("/create_photo", { :controller => "photos", :action => "create_row"})
+
+#read
+get("/photos/#:the_id_number", { :controller => "photos", :action => "show"})
 get("/photos", { :controller => "photos", :action => "index"})
+
+#update
+get("/photos/:the_id/edit", { :controller => "photos", :action => "edit_form"})
+get("/update_photo/:the_id", { :controller => "photos", :action => "update_row"})
+
+#delete
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
